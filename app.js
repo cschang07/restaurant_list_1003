88ac7c55564c8de654b1ea01f6e16cad5fe4f180
cschang07 <manutdsfan@gmail.com>
2021-10-06 10:20:48 +0800

    remove unneccesary comment

--- app.js
+++ app.js
@@ -27,19 +27,3 @@ app.use(express.static('public'))
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
-
-// const mongoose = require('mongoose') // 載入 mongoose
-
-// //connect to Database
-// mongoose.connect('mongodb://localhost/restaurant_list_0923', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-// // 取得資料庫連線狀態
-// const db = mongoose.connection
-// // 連線異常
-// db.on('error', () => {
-//   console.log('mongodb error!')
-// })
-// // 連線成功
-// db.once('open', () => {
-//   console.log('mongodb connected!')
-// })
