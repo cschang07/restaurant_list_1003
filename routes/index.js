bc65a593bf607ed9947ebc7b5fdcb2f82e102805
cschang07 <manutdsfan@gmail.com>
2021-10-21 17:49:54 +0800

    feat: add login/register feature

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-// 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
 
-// 引入模組程式碼
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router
